6ccc65ca0d08ab3375282a2cd8e38f6ae47fc4aa
Amit Vyas <amit@5exceptions.com>
2023-01-17 20:03:09 +0530

    Created Get employee list

--- src/main/java/com/fiveExceptions/controller/EmployeeController.java
+++ src/main/java/com/fiveExceptions/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.fiveExceptions.controller;
 
 import com.fiveExceptions.dto.EmployeeRequest;
+import com.fiveExceptions.dto.EmployeeResponse;
 import com.fiveExceptions.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -17,5 +20,11 @@ public class EmployeeController {
     @ResponseStatus(HttpStatus.CREATED)
     public void createEmployee(@RequestBody EmployeeRequest employeeRequest) {
         employeeService.createEmployee(employeeRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<EmployeeResponse> getAllEmployee() {
+        return employeeService.getAllEmployee();
     }
 }
--- src/main/java/com/fiveExceptions/service/EmployeeService.java
+++ src/main/java/com/fiveExceptions/service/EmployeeService.java
@@ -1,11 +1,14 @@
 package com.fiveExceptions.service;
 
 import com.fiveExceptions.dto.EmployeeRequest;
+import com.fiveExceptions.dto.EmployeeResponse;
 import com.fiveExceptions.entity.Employee;
 import com.fiveExceptions.repository.EmployeeRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -25,4 +28,22 @@ public class EmployeeService {
         log.info("Employee {} is saved", employee.getId());
 
     }
+
+    public List<EmployeeResponse> getAllEmployee() {
+        List<Employee> employees = employeeRepository.findAll();
+
+        return employees.stream().map(this::mapToEmployeeResponse) .toList();
+    }
+
+    private EmployeeResponse mapToEmployeeResponse(Employee employee) {
+        return EmployeeResponse.builder()
+                .id(employee.getId())
+                .firstName(employee.getFirstName())
+                .lastName(employee.getLastName())
+                .email(employee.getEmail())
+                .department(employee.getDepartment())
+                .build();
+    }
+
+
 }
